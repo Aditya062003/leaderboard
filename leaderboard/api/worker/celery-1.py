8e649cb93addc4f7964c7c74833de1a2771f9aef
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-06-08 11:52:08 +0530

    Delete leaderboard/api/worker directory

--- leaderboard/api/worker/celery-1.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-
-app = Celery('celery-1', broker='redis-18982.c80.us-east-1-2.ec2.cloud.redislabs.com:18982', backend='redis-18982.c80.us-east-1-2.ec2.cloud.redislabs.com:18982')
-
-@app.task
-def celery_1_task():
-    celery -A leaderboard worker --loglevel=info
-    celery -A leaderboard beat -l info
-    pass
