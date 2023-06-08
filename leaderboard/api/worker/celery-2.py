7a732bfa3e36f70bee6ffcdd08cef942145cccb1
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-06-08 11:43:10 +0530

    Delete celery-2.py

--- leaderboard/api/worker/celery-2.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-app = Celery('celery-2', broker='redis-18982.c80.us-east-1-2.ec2.cloud.redislabs.com:18982', backend='redis-18982.c80.us-east-1-2.ec2.cloud.redislabs.com:18982')
-
-@app.task
-def celery_2_task():
-    celery -A leaderboard beat -l info
-    pass
